979662e61bf95ade84b3f87832194a165fd7c76a
Thomas Chardonnens <61554870+ThomasCdnns@users.noreply.github.com>
2022-01-21 19:33:29 +0100

    sketch final on screen

---
+++ sketch_final/FFT_code_Short.ino
@@ -0,0 +1,116 @@
+//------------------------------------------------------------------------------
+// FFT_code_Short.c
+// Implementation of a complex fft originally written by Rulph Chassaing.
+// Reworked for the Cortex-M4 by Mark Wickert, April 2015.
+//
+// Reworked by B. BARRY  May 2020
+//------------------------------------------------------------------------------
+
+#include	<math.h>
+
+
+#define		MAX_FFT_LEN		512
+
+
+struct cmpx {			//complex data structure used by FFT
+	float real, imag;
+};
+typedef struct cmpx COMPLEX;
+
+
+void	make_twiddle_array(int fftlen);
+
+COMPLEX		Yvec[MAX_FFT_LEN], Twiddle[MAX_FFT_LEN];
+
+
+int	fft_abs_short(short *Sig_Vect, float *Fft_Vect, int fftlen)
+//----------------------------------------------------------------------
+// Parametres en entree :
+//	Sig_Vect = vecteur echantillons du signal
+//	fftlen      = nombre de points de la FFT
+// Parametres en sortie :
+//	 Fft_Vect = vecteur echantillons du module de la FFT (abs(fft))
+//----------------------------------------------------------------------
+{
+	COMPLEX  temp1, temp2;		//temporary storage variables
+	float vreal, vimag;
+	int i, j, k;				//loop counter variables
+	int upper_leg, lower_leg;	//index of upper/lower
+	//butterfly leg
+	int leg_diff;				//difference between upper/lower leg
+	int index, step;			//index and step between twiddle factor
+	int num_stages = 0;			//number of FFT stages, or iterations
+	int nerr = 0;
+
+	if (fftlen > MAX_FFT_LEN || fftlen < 64) {
+		nerr = 1;	goto sortie;
+	}
+	// Convert real Sig_Vect into complex Yvec
+	for (i = 0; i < fftlen; i++) {
+		Yvec[i].real = (float)Sig_Vect[i];		Yvec[i].imag = 0.0;
+	}
+
+	make_twiddle_array(fftlen);
+	i = 1;						//log(base 2) of # of points = # of stages
+	do 	{
+		num_stages += 1;
+		i = i * 2;
+	} while (i < fftlen);
+
+	leg_diff = fftlen / 2;		//starting difference between pper & lower legs
+	step = 2;					//step between values in twiddle.h
+	for (i = 0; i < num_stages; i++) { //for M-point FFT
+		index = 0;
+		for (j = 0; j < leg_diff; j++) {
+			for (upper_leg = j; upper_leg < fftlen; upper_leg += (2*leg_diff)) {
+				lower_leg = upper_leg + leg_diff;
+				temp1.real = (Yvec[upper_leg]).real + (Yvec[lower_leg]).real;
+				temp1.imag = (Yvec[upper_leg]).imag + (Yvec[lower_leg]).imag;
+				temp2.real = (Yvec[upper_leg]).real - (Yvec[lower_leg]).real;
+				temp2.imag = (Yvec[upper_leg]).imag - (Yvec[lower_leg]).imag;
+				(Yvec[lower_leg]).real = temp2.real*(Twiddle[index]).real - temp2.imag*(Twiddle[index]).imag;
+				(Yvec[lower_leg]).imag = temp2.real*(Twiddle[index]).imag + temp2.imag*(Twiddle[index]).real;
+				(Yvec[upper_leg]).real = temp1.real;
+				(Yvec[upper_leg]).imag = temp1.imag;
+			}
+			index += step;
+		}
+		leg_diff = leg_diff / 2;
+		step *= 2;
+	}
+	j = 0;
+	for (i = 1; i < (fftlen - 1); i++) { //bit reversal for	//re-sequencing data
+		k = fftlen / 2;
+		while (k <= j) {
+			j = j - k;
+			k = k / 2;
+		}
+		j = j + k;
+		if (i < j) {
+			temp1.real = (Yvec[j]).real;
+			temp1.imag = (Yvec[j]).imag;
+			(Yvec[j]).real = (Yvec[i]).real;
+			(Yvec[j]).imag = (Yvec[i]).imag;
+			(Yvec[i]).real = temp1.real;
+			(Yvec[i]).imag = temp1.imag;
+		}
+	}
+	for (i = 0; i < fftlen; i++) {
+		vreal = Yvec[i].real;		vimag = Yvec[i].imag;
+		Fft_Vect[i] = (float)sqrt(vreal*vreal + vimag*vimag);
+	}
+sortie:
+	return nerr;
+}
+
+void	make_twiddle_array(int fftlen)
+//-------------------------------------------------------------
+//-------------------------------------------------------------
+{	int n;	float vn;
+
+	for (n = 0; n < fftlen; n++) { //set up DFT twiddle factors
+		vn = (float) (PI*n/fftlen);
+		Twiddle[n].real = (float)cos(vn);
+		Twiddle[n].imag = (float)(-sin(vn));
+	}
+}
--- sketch_final/sketch_final.ino
+++ sketch_final/sketch_final.ino
@@ -1,5 +1,29 @@
 #include "Iseplogo128.h"
 
+//Detection de son pénible
+#define  RCV_TIM_LIMIT  10000000
+#define MY_LEDV   33
+#define MY_LEDJ   33 
+#define MY_INPUT  18 
+#define GIL_SW2  11
+#define GIL_SW1  12
+#define GIL_BUTTON 10
+
+#define BUF_SIZE  500     // size of the buffer containing the input samples. Must be greater than the order of the filters + K
+#define KP 50            // number of samples for the estimation of the signal power
+#define S 200// to define          // Threshold applied to the signal power  for the signal detection
+
+float buf[BUF_SIZE];      // Buffer containing the input samples
+float pIn;                // input power
+
+unsigned long current_time = 0;
+unsigned long current_time0 = 0;
+unsigned long next_sample_time = 0;
+unsigned long delta[20];
+unsigned int  Ts = 500;
+
+int compteur;
+
 //Humidity Sensor
 int DHpin = A2;
 byte dat [5];
@@ -37,8 +61,22 @@ int displayValue = 72;
 int displayUnit = 100; 
 
 void setup() {
-  // put your setup code here, to run once:
+  int i;
   Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(MY_LEDJ, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  
+  for (i = 0; i < BUF_SIZE; i++)
+  {
+    buf[i]  = 0;
+  }
+  pIn = 0;
+  compteur = 0;
+  delay(1);
+  compteur = 0;
+  digitalWrite(MY_LEDJ, LOW);
+  
   pinMode (DHpin, OUTPUT);
   InitI2C();
   InitScreen();
@@ -56,14 +94,12 @@ void setup() {
 void loop() {
   //CO2 Sensor
   int co2Value = analogRead(A2);
-  Serial.println(co2Value);
   DisplayInt(displayValue, 41, int(co2Value/4));
   DisplayString(displayUnit, 41, "ppm");
 
   //Micro Sensor
   int microValue = analogRead(A3);
   int microValueToDbm = 10*log10(microValue*1000);
-  Serial.println(microValueToDbm);
   DisplayInt(displayValue,42, microValueToDbm);
   DisplayString(displayUnit, 42, "dBm");
 
@@ -71,7 +107,6 @@ void loop() {
   int tempValue = analogRead(A3);
   //int tempFinalValue = tempValue * (3.3 / 4096.0);
   int tempFinalValue = 21;
-  Serial.println(tempFinalValue);
   DisplayInt(displayValue,43,tempFinalValue);
   DisplayString(displayUnit,43,"C");
 
@@ -86,9 +121,53 @@ void loop() {
   //Heart rate
   //int heartRateValue = analogRead(A3);
   int heartRateValue = 89;
-  Serial.println(heartRateValue);
   DisplayInt(displayValue,45, heartRateValue);
   DisplayString(displayUnit, 45, "BPM");
+
+  Serial.println(String(co2Value) + " " + String(microValueToDbm) + " " + String(tempFinalValue) + " " + String(humidityValue) + " " + String(heartRateValue));
+  Serial1.println(String(co2Value) + " " + String(microValueToDbm) + " " + String(tempFinalValue) + " " + String(humidityValue) + " " + String(heartRateValue));
+  //Détection Son pénible 
+  int i,n;
+  // Read data : shift the previous samples in the buffer ...
+  for (i = BUF_SIZE - 2; i >= 0; i--)
+  {
+    //buf[i] = buf[i-1]; // To be completed
+    buf[i] = buf[i-1];
+  }
+  // ... and acquire the new sample
+  current_time = micros() ;
+  while (current_time < next_sample_time)
+  {
+    current_time  = micros();                  //To be completed
+  }
+  buf[0] = (float) analogRead(MY_INPUT) - 2048.0;
+
+  // Define the next sampling time
+  next_sample_time += (unsigned long)Ts;
+  // Update "instantaneous" power
+  pIn = 0;
+  for (i = 0; i < KP; i++)
+  {
+    pIn = pIn + pow(buf[i],2);                   //-- to be completed
+  }
+  pIn = pIn / (float)KP;
+  // Display ... sommetimes - be careful, it disturbs the sampling interval
+  if (compteur == 200)
+  {
+    Serial.println(pIn);
+    compteur = 0;
+  }
+  compteur ++;
+  // Test the amplitude of the instantaneous power
+  if (pIn > S)
+  {
+       digitalWrite(MY_LEDJ, HIGH);   // Switch on the LED to indicate that an audio signal is detected
+  }
+  else      //  No useful audio signal detected
+  {
+      digitalWrite(MY_LEDJ, LOW);
+  }
+
 
   delay(500);
   
